699622a02ee73bd174470e9d85125244c0ef6a9b
xinhan2023 <144680992+xinhan2023@users.noreply.github.com>
2026-01-16 01:11:31 +0800

    Add files via upload

--- SNP calling/1ref.sh
+++ SNP calling/1ref.sh
@@ -7,7 +7,7 @@ picard=/software/picard.jar
 
 iTools Fatools regenerate -InPut gyr_scaffold.fa -OutPut gyr.newchr.fa -InsertN 500 -Length 30000000 -NoOutgz
 $bwa index gyr.newchr.fa -p gyr.newchr.fa
-java -Djava.io.tmpdir=/beegfs/liuxin/saker/temp -Xmx4g -jar $picard CreateSequenceDictionary \
+java -Djava.io.tmpdir=temp -Xmx4g -jar $picard CreateSequenceDictionary \
 	R=gyr.newchr.fa \
 	O=gyr.newchr.dict
 samtools faidx gyr.newchr.fa
